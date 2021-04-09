b0c24be32241c6e1fa9bf78186b6fe390a35d46d
Alaa Sobhy <bosy.sobhy193@gmail.com>
2021-04-10 01:22:19 +0200

    solve map issue

--- lib/authentication/booking.dart
+++ lib/authentication/booking.dart
@@ -20,17 +20,10 @@ class Booking extends StatefulWidget {
 
 class _BookingState extends State<Booking> {
   Map<DateTime, List<dynamic>> _events = {};
-  Completer<GoogleMapController> _controller = Completer();
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
+  var myMarker = Marker(
+    markerId: MarkerId("1"),
   );
 
-  static final CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
   @override
   Widget build(BuildContext context) {
     Completer<GoogleMapController> _controller = Completer();
@@ -310,17 +303,17 @@ class _BookingState extends State<Booking> {
         //     ),
         //   ),
         // ),
-        // Container(
-        //   height: 500,
-        //   width: 500,
-        //   child: GoogleMap(
-        //     mapType: MapType.hybrid,
-        //     initialCameraPosition: _kGooglePlex,
-        //     onMapCreated: (GoogleMapController controller) {
-        //       _controller.complete(controller);
-        //     },
-        //   ),
-        // ),
+        Container(
+          height: 500,
+          width: 500,
+          child: GoogleMap(
+            mapType: MapType.normal,
+            initialCameraPosition: _kGooglePlex,
+            onMapCreated: (GoogleMapController controller) {
+              _controller.complete(controller);
+            },
+          ),
+        ),
       ],
     ));
   }
