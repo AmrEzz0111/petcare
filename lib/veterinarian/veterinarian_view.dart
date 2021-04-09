601cc5628e244d2cd5b589782b87a65ca7d01439
Alaa Sobhy <bosy.sobhy193@gmail.com>
2021-04-09 15:30:27 +0200

    veterinarian

--- lib/veterinarian/veterinarian_view.dart
+++ lib/veterinarian/veterinarian_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:pet_care/colors/style.dart';
-import 'package:pet_care/widgets/appBar.dart';
 import 'package:pet_care/widgets/veterinarian_item.dart';
 
 class VeterinarianView extends StatefulWidget {
@@ -8,54 +7,163 @@ class VeterinarianView extends StatefulWidget {
   _VeterinarianViewState createState() => _VeterinarianViewState();
 }
 
-class _VeterinarianViewState extends State<VeterinarianView> {
+class _VeterinarianViewState extends State<VeterinarianView>
+    with TickerProviderStateMixin {
+  TabController controller;
+  @override
+  void initState() {
+    controller = new TabController(length: 2, vsync: this);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[100],
       appBar: PreferredSize(
-        preferredSize: Size.fromHeight(50),
-        child: AppBarWidget(
-          leading: InkWell(
-            onTap: () => Navigator.pop(context),
-            child: Icon(
-              Icons.arrow_back_rounded,
-              color: Colors.grey,
-            ),
-          ),
-          evaluation: 0.5,
-          title: "",
-          actions: [
-            Padding(
-              padding: const EdgeInsets.only(right: 15),
-              child: InkWell(
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.location_on_outlined,
-                      color: AppTheme.appDark,
-                      size: 20,
+          preferredSize: Size.fromHeight(130),
+          child: Stack(
+            children: [
+              AppBar(
+                backgroundColor: Colors.white,
+                leading: InkWell(
+                  onTap: () => Navigator.pop(context),
+                  child: Icon(
+                    Icons.arrow_back_rounded,
+                    color: Colors.grey,
+                  ),
+                ),
+                shadowColor: Colors.grey[300],
+                shape: ContinuousRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(80),
+                      bottomRight: Radius.circular(80)),
+                ),
+                elevation: 0,
+                actions: [
+                  Padding(
+                    padding: const EdgeInsets.only(right: 16),
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.location_on_outlined,
+                          color: AppTheme.appDark,
+                          size: 20,
+                        ),
+                        Text(
+                          'Map',
+                          style: TextStyle(
+                            color: AppTheme.appDark,
+                            fontFamily: 'coRegular',
+                            fontSize: 14,
+                          ),
+                        ),
+                      ],
                     ),
-                    Text(
-                      'Map',
+                  ),
+                ],
+                title: buildTabbar(),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 100, right: 16, left: 16),
+                child: Container(
+                  height: 40,
+                  child: TextField(
+                      controller: TextEditingController(),
+                      cursorColor: AppTheme.appDark,
                       style: TextStyle(
-                        color: AppTheme.appDark,
+                        color: Colors.black87,
+                        fontSize: 14.0,
                         fontFamily: 'coRegular',
-                        fontSize: 14,
                       ),
-                    ),
-                  ],
+                      decoration: InputDecoration(
+                          fillColor: Colors.grey[200],
+                          filled: true,
+                          contentPadding: EdgeInsets.symmetric(vertical: 5),
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                            color: AppTheme.appDark,
+                            width: 1.0,
+                          )),
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                            color: Colors.grey[200],
+                            width: 1.0,
+                          )),
+                          hintText: 'Veterinarian',
+                          hintStyle: TextStyle(
+                            color: AppTheme.bgColor,
+                            fontSize: 14.0,
+                            fontFamily: 'coRegular',
+                            fontWeight: FontWeight.bold,
+                          ),
+                          prefixIcon: Icon(
+                            Icons.search,
+                            color: AppTheme.bgColor,
+                          ))),
+                ),
+              ),
+            ],
+          )),
+      body: buildExpandedTabBarView(),
+    );
+  }
+
+  buildTabbar() {
+    return Container(
+      height: 33,
+      decoration: BoxDecoration(
+          border: Border.all(
+            color: Color(0xff002E5F).withOpacity(0.14),
+          ),
+          borderRadius: BorderRadius.circular(50),
+          color: Colors.white),
+      child: TabBar(
+          isScrollable: true,
+          controller: controller,
+          unselectedLabelColor: AppTheme.bgColor,
+          indicatorSize: TabBarIndicatorSize.tab,
+          indicator: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(50)),
+              color: AppTheme.appDark),
+          tabs: [
+            Tab(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 5, right: 5, top: 3),
+                child: Text(
+                  'Specialists',
+                  style: TextStyle(
+                    fontFamily: 'coRegular',
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
             ),
-          ],
-        ),
-      ),
-      body: ListView.builder(
-          itemCount: 3,
-          itemBuilder: (BuildContext context, int index) {
-            return VeterinarianItem();
-          }),
+            Tab(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 5, right: 5, top: 3),
+                child: Text(
+                  'Clinics',
+                  style: TextStyle(
+                    fontFamily: 'coRegular',
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ]),
+    );
+  }
+
+  buildExpandedTabBarView() {
+    return TabBarView(
+      controller: controller,
+      children: <Widget>[
+        VeterinarianItem(),
+        VeterinarianItem(),
+      ],
     );
   }
 }
--- lib/widgets/veterinarian_item.dart
+++ lib/widgets/veterinarian_item.dart
@@ -5,176 +5,178 @@ import 'package:pet_care/colors/style.dart';
 class VeterinarianItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Container(
-        height: 150,
-        decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(10),
-            boxShadow: [
-              BoxShadow(
-                  color: Colors.grey[200],
-                  offset: Offset(5.0, 5.0),
-                  blurRadius: 5,
-                  spreadRadius: 5),
-            ]),
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Container(
-                    height: 80,
-                    width: 80,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        image: DecorationImage(
-                            fit: BoxFit.cover,
-                            image: AssetImage('assets/images/DrVet.jpg'))),
-                  ),
-                  SizedBox(
-                    width: 20,
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Peternko Julia',
-                        style: TextStyle(
-                            color: AppTheme.bgColor,
-                            fontFamily: 'coRegular',
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold),
-                      ),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      Text(
-                        'Veterinarian',
-                        style: TextStyle(
-                            color: AppTheme.appDark,
-                            fontFamily: 'coRegular',
-                            fontSize: 14,
-                            fontWeight: FontWeight.bold),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Row(
-                        children: [
-                          RatingBar(
-                            initialRating: 4.5,
-                            direction: Axis.horizontal,
-                            allowHalfRating: true,
-                            ignoreGestures: true,
-                            itemSize: 14,
-                            itemCount: 5,
-                            ratingWidget: RatingWidget(
-                              full: Icon(
-                                Icons.star,
-                                color: Colors.amber,
+    return ListView.builder(itemBuilder: (BuildContext context, int index) {
+      return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+        child: Container(
+          height: 150,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey[200],
+                    offset: Offset(5.0, 5.0),
+                    blurRadius: 5,
+                    spreadRadius: 5),
+              ]),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      height: 80,
+                      width: 80,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: AssetImage('assets/images/DrVet.jpg'))),
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Peternko Julia',
+                          style: TextStyle(
+                              color: AppTheme.bgColor,
+                              fontFamily: 'coRegular',
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          'Veterinarian',
+                          style: TextStyle(
+                              color: AppTheme.appDark,
+                              fontFamily: 'coRegular',
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          children: [
+                            RatingBar(
+                              initialRating: 4.5,
+                              direction: Axis.horizontal,
+                              allowHalfRating: true,
+                              ignoreGestures: true,
+                              itemSize: 14,
+                              itemCount: 5,
+                              ratingWidget: RatingWidget(
+                                full: Icon(
+                                  Icons.star,
+                                  color: Colors.amber,
+                                ),
+                                half: Icon(
+                                  Icons.star,
+                                  color: Colors.black12,
+                                ),
+                                empty: Icon(
+                                  Icons.star,
+                                  color: Colors.black12,
+                                ),
                               ),
-                              half: Icon(
-                                Icons.star,
-                                color: Colors.black12,
-                              ),
-                              empty: Icon(
-                                Icons.star,
-                                color: Colors.black12,
-                              ),
+                              onRatingUpdate: (rating) {
+                                print(rating);
+                              },
                             ),
-                            onRatingUpdate: (rating) {
-                              print(rating);
-                            },
-                          ),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          Text(
-                            '125 Reviews',
-                            style: TextStyle(
-                                color: Colors.grey[400],
-                                fontFamily: 'coRegular',
-                                fontSize: 12,
-                                fontWeight: FontWeight.w600),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              Row(
-                children: [
-                  Text(
-                    '10 year of experience ',
-                    style: TextStyle(
-                        color: Colors.grey[400],
-                        fontFamily: 'coRegular',
-                        fontSize: 12,
-                        fontWeight: FontWeight.w600),
-                  ),
-                  SizedBox(
-                    width: 25,
-                  ),
-                  CircleAvatar(
-                    radius: 11,
-                    backgroundColor: Colors.grey[100],
-                    child: Center(
-                      child: Icon(
-                        Icons.location_on_outlined,
-                        color: Colors.grey[600],
-                        size: 17,
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text(
+                              '125 Reviews',
+                              style: TextStyle(
+                                  color: Colors.grey[400],
+                                  fontFamily: 'coRegular',
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w600),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      '10 year of experience ',
+                      style: TextStyle(
+                          color: Colors.grey[400],
+                          fontFamily: 'coRegular',
+                          fontSize: 12,
+                          fontWeight: FontWeight.w600),
+                    ),
+                    SizedBox(
+                      width: 25,
+                    ),
+                    CircleAvatar(
+                      radius: 11,
+                      backgroundColor: Colors.grey[100],
+                      child: Center(
+                        child: Icon(
+                          Icons.location_on_outlined,
+                          color: Colors.grey[600],
+                          size: 17,
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Text(
-                    '2.4 km',
-                    style: TextStyle(
-                        color: Colors.grey[600],
-                        fontFamily: 'coRegular',
-                        fontSize: 12,
-                        fontWeight: FontWeight.w600),
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  CircleAvatar(
-                    radius: 11,
-                    backgroundColor: Colors.grey[100],
-                    child: Center(
-                      child: Icon(
-                        Icons.payment_rounded,
-                        color: Colors.grey[600],
-                        size: 17,
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      '2.4 km',
+                      style: TextStyle(
+                          color: Colors.grey[600],
+                          fontFamily: 'coRegular',
+                          fontSize: 12,
+                          fontWeight: FontWeight.w600),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    CircleAvatar(
+                      radius: 11,
+                      backgroundColor: Colors.grey[100],
+                      child: Center(
+                        child: Icon(
+                          Icons.payment_rounded,
+                          color: Colors.grey[600],
+                          size: 17,
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Text(
-                    '\$20',
-                    style: TextStyle(
-                        color: Colors.grey[600],
-                        fontFamily: 'coRegular',
-                        fontSize: 12,
-                        fontWeight: FontWeight.w600),
-                  ),
-                ],
-              ),
-            ],
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      '\$20',
+                      style: TextStyle(
+                          color: Colors.grey[600],
+                          fontFamily: 'coRegular',
+                          fontSize: 12,
+                          fontWeight: FontWeight.w600),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
