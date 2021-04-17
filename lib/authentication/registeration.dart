1d427398c84add303696504d4ba57dfac2f6728e
Alaa Sobhy <alaa.sobhy@hamza.solutions>
2021-04-17 02:22:37 +0200

    google sign in

--- lib/authentication/registeration.dart
+++ lib/authentication/registeration.dart
@@ -4,6 +4,7 @@ import 'package:pet_care/authentication/authentication-provider.dart';
 import 'package:pet_care/authentication/sign_in_screen.dart';
 import 'package:pet_care/colors/style.dart';
 import 'package:pet_care/models/user_model.dart';
+import 'package:pet_care/profile/profile_view.dart';
 import 'package:pet_care/screens/home_screen.dart';
 import 'package:provider/provider.dart';
 
@@ -172,7 +173,7 @@ class _RegisteraionState extends State<Registeraion> {
                               if (signUpProv.user != null) {
                                 Navigator.of(context).pushReplacement(
                                   MaterialPageRoute(
-                                    builder: (context) => HomeScreen(
+                                    builder: (context) => ProfileView(
                                       user: signUpProv.user,
                                     ),
                                   ),
