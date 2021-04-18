77c24863eff729532ce28cec8681e8a3b9d88de9
adelahmed <aadil0384@gmail.com>
2021-04-19 00:06:36 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	lib/main.dart
    #	pubspec.lock
    #	pubspec.yaml

--- lib/repositories/auth-repository.dart
+++ lib/repositories/auth-repository.dart
@@ -1,8 +1,28 @@
+import 'dart:io';
+
+import 'package:pet_care/models/doctor_model.dart';
+import 'package:pet_care/models/user_model.dart';
 import 'package:pet_care/services/auth-service.dart';
 
 class AuthRepository {
   AuthService _authService = AuthService();
-  Future getSignInEmailAndPassword(String username, String password) {
-    return _authService.signInEmailAndPassword(username, password);
+  Future signInEmailAndPassword(String email, String password) {
+    return _authService.signInEmailAndPassword(email, password);
+  }
+
+  Future signInWithGoogle() {
+    return _authService.signInWithGoogle();
+  }
+
+  Future signUp(String email, String password, UserModel user) {
+    return _authService.signUp(email, password, user);
+  }
+
+  Future signUpAsDoctor(String email, String password, Doctor user) {
+    return _authService.signUpAsDoctor(email, password, user);
+  }
+
+  Future updateUser(UserModel user, File img) {
+    return _authService.updateUser(user, img);
   }
 }
